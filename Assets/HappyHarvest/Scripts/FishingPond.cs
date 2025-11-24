a786da4a12fd3f9d5ef2ea7b391e1818dc07ff6c
KillerType59 <144765417+KillerType59@users.noreply.github.com>
2025-11-24 22:34:31 +0800

    implement basic UI

--- Assets/HappyHarvest/Scripts/FishingPond.cs
+++ Assets/HappyHarvest/Scripts/FishingPond.cs
@@ -1,34 +1,10 @@
-using System.Collections;
-using UnityEngine;
-using TMPro;
 namespace HappyHarvest
 {
     public class FishingPond : InteractiveObject
     {
-        [SerializeField]
-        private GameObject pondHint;
-        private Coroutine showHint;
-
         public override void InteractedWith()
         {
-            if (showHint != null)
-            {
-                StopCoroutine(showHint);
-            }
-
-            showHint = StartCoroutine(ShowHint());
-        }
-
-        IEnumerator ShowHint()
-        {
-            pondHint.SetActive(true);
-
-            yield return new WaitForSeconds(2.0f);
-
-            pondHint.SetActive(false);
-            GameManager instance = GameManager.Instance;
-            instance.Player.AddItem(instance.ItemDatabase.GetFromID("fih_calling"));
+            UIHandler.OpenFishingSpot();
         }
     }
-
 }
